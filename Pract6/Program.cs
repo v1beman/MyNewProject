7fdb64a713c85ffc236b57cbd827d9ad91abc4b9
v1beman <gruzdev-05@list.ru>
2022-11-22 20:18:59 +0300

    свыыв

---
+++ Pract6/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace Test
+{
+
+    public class Program
+    {
+        [Serializable]
+        public class Figures
+        {
+            public string Shape { get; set; }
+            public int Length { get; set; }
+            public int Width { get; set; }
+
+            public override string ToString()
+            {
+                return Shape + "\n" + Length + "\n" + Width;
+            }
+
+            public Figures(string a, int b, int c)
+            {
+                Shape = a; Length = b; Width = c;
+            }
+
+        }
+        static List<Figures> figures { get; set; } = new List<Figures>();
+
+        static void Main(string[] args)
+        {
+           // vod();
+
+            Console.WriteLine("Введите путь до файла");
+            Console.WriteLine("---------------------");
+
+            string way = Console.ReadLine();
+
+
+            if (way.Contains("txt"))
+            {
+                Console.Clear();
+                Console.WriteLine("Для сохранения файла нажмите F1; Для выхода намжмите Escape");
+                Console.WriteLine("-----------------------------------------------------------");
+                string[] lines = File.ReadAllLines(way);
+                for(int i = 0; i < lines.Length; i + 3)
+                {
+                    Figures a = new Figures(lines[i]);
+                       Convert.ToInt32( lines[i + 1]);
+                         Convert.ToInt32( lines[i + 2]); 
+                    figures.Add(a);
+                }
+
+
+            }
+            else if (way.Contains("json"))
+            {
+                string json = File.ReadAllText(way);
+                figures = JsonConvert.DeserializeObject<List<Figures>>(json);
+
+                Console.Clear();
+                Console.WriteLine("Для сохранения файла нажмите F1; Для выхода намжмите Escape");
+                Console.WriteLine("-----------------------------------------------------------");
+
+                foreach (var abv in figures)
+                {
+                    Console.WriteLine(abv);
+                }
+
+            }
+            else if (way.Contains(".xml"))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(List<Figures>));
+                using (FileStream fs = new FileStream(way, FileMode.Open))
+                {
+                    figures = (List<Figures>)xml.Deserialize(fs);
+
+
+                    foreach (var abc in figures)
+                    {
+                        Console.WriteLine(abc);
+                    }
+                }
+            }
+
+
+
+            vod();
+        }
+
+
+        static void vod()
+        {
+            ConsoleKeyInfo a = Console.ReadKey();
+            if (a.Key == ConsoleKey.F1)
+            {
+
+                Console.Clear();
+                Console.WriteLine("Введите путь до файла, куда вы хотите сохранить текст");
+                Console.WriteLine("-----------------------------------------------------");
+                string trap = Console.ReadLine()!;
+                if (trap.Contains(".txt"))
+                {
+                    StreamWriter sw = new StreamWriter(trap);
+                    foreach (var figure in figures)
+                    {
+                        sw.WriteLine(figure);
+                    }
+                    sw.Close();
+                }
+                else if (trap.Contains(".json"))
+                {
+                    string json = JsonConvert.SerializeObject(figures);
+                    StreamWriter sw = new StreamWriter(trap);
+                    sw.WriteLine(json);
+                    sw.Close();
+                }
+                else if (trap.Contains(".xml"))
+                {
+                    XmlSerializer xml = new XmlSerializer(typeof(List<Figures>));
+                    using (FileStream fs = new FileStream(trap, FileMode.OpenOrCreate))
+                    {
+                        xml.Serialize(fs, figures);
+
+                    }
+                }
+            }
+
+        }
+    }
+}
